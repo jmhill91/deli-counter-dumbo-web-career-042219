863802f199d710b9393b85c0ef3446a7edfeeb13
John Hill <john.m.hill91@gmail.com>
2019-03-25 21:34:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 def line(arr)
-  line="The line is currently: "
+  line="The line is currently:"
   if arr.length  == 0 
     puts "The line is currently empty."
     elsif arr.length != 0 
     arr.each_with_index do |name, index|
-   line += "#{index+1}. #{name}"
+   line += " #{index+1}. #{name}"
     end
     puts line
 end
