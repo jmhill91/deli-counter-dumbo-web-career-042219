9e12838c14e7cbe3775cdfe09bec13015cf7b736
John Hill <john.m.hill91@gmail.com>
2019-03-25 21:30:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(arr)
+  count=1
+  line=["The line is currently: "]
+  if arr.length  == 0 
+    puts "The line is currently empty."
+    elsif arr.length != 0 
+    arr.each do 
+   line.push( "#{count}. #{i}")
+    end
+    puts line
+end
+end
